2e0b03a8967a36471e9af350ab11be4d64e97c69
Sadim <148269014+Sadim-Verse@users.noreply.github.com>
2025-05-20 15:49:58 +0100

    Update - title menu
    I added some kind of pulse to the game_title
    I added some simple 'rain' fx into the title scene

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -1,9 +1,17 @@
 extends Control
 
+# The background image can & should be changed
 
 # A textureRect can be used in place of a "Label" if the name of the game is some image or texture file, or if you'd want to use a logo instead of a game name
 
-# The background image can & should be changed
+# necessary for adding a 'pulse' to the game title text
+@onready var game_title := $title_text
+
+
+var pulse_speed := 2.0			# determines how fast the pulse will be 
+var pulse_scale := 0.05			# determines how big/small the pulse will get
+var time_passed := 0.0			# determines how much time has passed since game start
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +20,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	scale_title_text(delta)
+
+func scale_title_text(delta):
+	# adds up time as the game runs
+	time_passed += delta
+	
+	# calculates and controls the scale(expansion & contraction) of the text through the sin()
+	var scale_factor = 1.0 + sin(time_passed * pulse_speed) * pulse_scale
+	
+	# uses the 'scale_factor' to set how the the label is
+	game_title.scale = Vector2(scale_factor, scale_factor)
 
 
 func _on_start__pressed():
